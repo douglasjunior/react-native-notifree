fbae39d45057d7ba70fe79a929cfbb7f24439d3a
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2021-05-05 08:33:59 -0300

    add ios native source

---
+++ ios/ReactNativeNotifreeLibrary/RNFirebaseEvents.h
@@ -0,0 +1,11 @@
+#ifndef RNFirebaseEvents_h
+#define RNFirebaseEvents_h
+
+#import <Foundation/Foundation.h>
+
+// Notifications
+static NSString *const NOTIFICATIONS_NOTIFICATION_DISPLAYED = @"notifications_notification_displayed";
+static NSString *const NOTIFICATIONS_NOTIFICATION_OPENED = @"notifications_notification_opened";
+static NSString *const NOTIFICATIONS_NOTIFICATION_RECEIVED = @"notifications_notification_received";
+
+#endif
